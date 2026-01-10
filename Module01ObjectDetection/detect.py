dfe2140d8a08d653a562226246216cb13b61c9ba
Kittipong <kittipong@gmail.com>
2026-01-10 21:27:39 +0700

    update module01

--- Module01ObjectDetection/detect.py
+++ Module01ObjectDetection/detect.py
@@ -4,11 +4,16 @@ from ultralytics import YOLOWorld
 
 model = YOLOWorld('Module01ObjectDetection/models/yolov8s-world.pt') #yolo8 model
 
-model.set_classes(["rectangle", "plate", "object"]) #promt
+model.set_classes(["rectangle", "plate", "object"]) #prompt
 
-image_path = "Module01ObjectDetection/images/input.jpg" #for testing
+image_name = "input1.jpg" #for testing
+
+image_path = f"Module01ObjectDetection/images/{image_name}" 
 image = cv2.imread(image_path)
-results = model.predict(image, conf= 0.005)
+results = model.predict(image, conf=0.0008, iou=0.45, max_det=1) 
+#imgsz(int) = size of image for detection
+#iou(double) = intersection over union threshold
+#max_det(int) = maximum number of detections per image
 
 output_dir = "Module01ObjectDetection/cropped_objects"
 os.makedirs(output_dir, exist_ok=True)
@@ -27,12 +32,13 @@ for i, result in enumerate(results):
         
         cropped_img = image[ny1:ny2, nx1:nx2]
         
-        file_name = f"{output_dir}/object_{i}_{j}.jpg" #for testing
+        file_name = f"{output_dir}/object_{i}_{j}_from_{image_name}" #for testing
         cv2.imwrite(file_name, cropped_img)
         print(f"Saved: {file_name}")
 
         cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
-#cv2.imshow("Detection Result", image) #for show areas
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+#for show areas of detection
+#cv2.imshow("Detection Result", image)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
