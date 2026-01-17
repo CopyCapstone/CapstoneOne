9c7ec8c8111d40f2a74af045b2031b0cb695118d
Prairiet <156326885+Prairiet@users.noreply.github.com>
2026-01-18 02:21:43 +0700

    rounded values

--- Module02ChromaticAdaptationTransform/cat.py
+++ Module02ChromaticAdaptationTransform/cat.py
@@ -2,7 +2,7 @@ import cv2
 from pathlib import Path
 import numpy as np
 
-image = f"{Path(__file__).parent}/test2.png"
+image = f"{Path(__file__).parent}/test1.png"
 
 data = cv2.imread(image)
 
@@ -11,7 +11,7 @@ mean_abs = mean_bgr.mean()
 print(mean_bgr)
 print(mean_abs)
 
-data_cat = (data * (mean_abs / mean_bgr)).clip(0, 255).astype(np.uint8)
+data_cat = (data * (mean_abs / mean_bgr)).round().clip(0, 255).astype(np.uint8)
 print(mean_abs / mean_bgr)
 
 # print(cat_image)
